697e852ad7593e1efc543bf6addf2577de8313f7
mrj025 <98994512+mrj025@users.noreply.github.com>
2022-06-20 01:37:27 -0300

    Add files via upload

---
+++ javascript/js.js
@@ -0,0 +1,19 @@
+function copy() {
+    alert("Autor: Martin Jofre.");
+    }
+
+    function inclusive()
+    {
+        document.body.style.backgroundColor= "black";
+        document.getElementById("header").style.backgroundColor = "black";
+        document.getElementById("header").style.color = "#90ffe9";
+        document.getElementById("nav").style.backgroundColor = "whitesmoke";
+        document.getElementById("nav").style.color = "black";
+        document.getElementById("section").style.backgroundColor = "black";
+        document.getElementById("section").style.color = "whitesmoke";
+        document.getElementById("aside").style.backgroundColor = "black";
+        document.getElementById("footer").style.backgroundColor = "black";
+        document.getElementById("aside").style.backgroundColor = "black";
+        document.getElementById("aside").style.backgroundColor = "white";
+
+    }
